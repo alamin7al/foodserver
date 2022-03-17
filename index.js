e46bb2e11493f2049710c92097bb69f3309d9830
alamin <ma7029582@gmail.com>
2022-03-17 12:16:39 +0600

    h

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const fileUpload = require('express-fileupload');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 
 // middleware 
 app.use(cors());
@@ -44,6 +44,7 @@ async function run() {
                 image: imageBuffer
             }
             const result = await datacollection.insertOne(doctor);
+            console.log(result);
             res.json(result);
         })
         app.get('/formdatas', async (req, res) => {
@@ -53,6 +54,9 @@ async function run() {
             const user = await cursor.toArray()
             res.json(user)
         })
+
+
+
         app.get('/orderplace', async (req, res) => {
             const email = req.query.email
             const query = { email: email }
@@ -60,6 +64,19 @@ async function run() {
             const user = await cursor.toArray()
             res.json(user)
         })
+
+        app.get('/allorderplace', async (req, res) => {
+            const cursor = ordercollection.find({})
+            const user = await cursor.toArray()
+            res.send(user)
+        })
+
+
+
+
+
+       
+
 
         app.get('/editprofile/:id', async (req, res) => {
             const id = req.params.id
@@ -127,12 +144,19 @@ async function run() {
                 payload
             });
         })
-        app.delete('/singleemail/:id', async (req, res) => {
+        app.delete('/allorderplace/:id', async (req, res) => {
             const id = req.params.id
             // console.log(id);
             const query = { _id: ObjectId(id) }
             const result = await ordercollection.deleteOne(query)
             console.log(result);
+            res.send(result)
+        })
+        app.delete('/singleemail/:id', async (req, res) => {
+            const id = req.params.id
+            // console.log(id);
+            const query = { _id: ObjectId(id) }
+            const result = await ordercollection.deleteOne(query)
             res.send(result)
         })
 
